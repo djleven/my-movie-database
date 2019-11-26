378ce5d78f80c2d3db3c94940d9787e9c69fc127
djleven <info@e-leven.net>
2019-11-26 17:03:35 +0200

    v2.0.2: add plugin activation status class to flush permalinks

--- my_movie_database.php
+++ my_movie_database.php
@@ -4,7 +4,7 @@
  * Plugin Name:       My Movie Database
  * Plugin URI:        https://wordpress.org/plugins/my-movie-database/
  * Description:       The My Movie Database plugin compliments your content by adding information about the movies, the television shows and the people you choose. The data comes from the Movie Database (TMDb). This plugin was developed to enrich your movie or tvshow critique / review by 'automatically' adding the related information and allowing you to focus on your writing instead.
- * Version:           2.0.1
+ * Version:           2.0.2
  * Author:            Kostas Stathakos
  * Author URI:        https://e-leven.net/
  * License:           GPL-2.0+
@@ -32,4 +32,44 @@ define( 'MMDB_ADVANCED_OPTION_GROUP', MMDB_PLUGIN_ID . '_opt_advanced' );
  */
 include_once plugin_dir_path( __FILE__ ) . 'includes/MyMovieDatabase.php';
 
+new ActivationStateChanges();
 new MyMovieDatabase\MyMovieDatabase();
+
+class ActivationStateChanges {
+
+    const PLUGIN_ACTIVATED = 'Plugin_Activated';
+
+    /**
+     * Define the activation and deactivation sequence tasks for the plugin.
+     *
+     * @since    2.0.2
+     */
+
+    public function __construct() {
+
+        register_activation_hook( __FILE__, array( $this, 'on_mmdb_activation') );
+//        register_deactivation_hook( __FILE__, array($this,'on_mmdb_deactivation'));
+        add_action( 'admin_init', array($this, 'on_load_mmdb') );
+    }
+
+
+    public function on_mmdb_activation() {
+
+        add_option( self::PLUGIN_ACTIVATED, MMDB_NAME );
+
+    }
+
+    public function on_mmdb_deactivation() {
+
+    }
+
+    public function on_load_mmdb() {
+
+        if ( is_admin() && get_option( self::PLUGIN_ACTIVATED ) ===  MMDB_NAME) {
+
+            delete_option( self::PLUGIN_ACTIVATED );
+
+            flush_rewrite_rules();
+        }
+    }
+}
