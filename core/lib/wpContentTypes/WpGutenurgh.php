fd1f0d08e83d5c855e83bd847ec8b6a1ca63709e
djleven <info@e-leven.net>
2020-07-19 16:20:06 -0400

    new barnch changes

---
+++ core/lib/wpContentTypes/WpGutenurgh.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The class that defines the mmdb post content
+ *
+ * The WpPostContentType is a subclass of the abstract WpAbstractContentType class.
+ *
+ * @link       https://e-leven.net/
+ * @since      1.0.0
+ *
+ * @package    my-movie-database
+ * @subpackage my-movie-database/core/lib/wpContentTypes
+ */
+namespace MyMovieDatabase\Lib\WpContentTypes;
+
+class WpGutengurgh extends WpAbstractContentType {
+
+}
